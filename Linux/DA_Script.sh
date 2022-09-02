538adbb39a103bf5a666ff013e0506ff31000b67
Valentin <96162706+1984err@users.noreply.github.com>
2022-09-01 21:38:54 -0400

    Delete Linux directory

--- Linux/DA_Script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Instead of making multiple scripts I used one to pull data from the player analysis notes to call the times dates for the script.
-
-# ex. [./DA_Script.sh 1 0310]  
-
-awk -F" " '{print $2" " $3}' ../Player_Analysis/Notes_Player_Analysis | sed -n $1p > temp_time.txt 
-awk -F" " '{print $1" " $2" " $5" "$6}' $2_Dealer_schedule | grep -f temp_time.txt >> Dealers_working_during_losses
-
--- Linux/roulette_dealer_finder_by_time.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat $1_Dealer_schedule | awk -F" " '{print $1,$2,$5,$6}'| grep "$2:00:00 $3M"
